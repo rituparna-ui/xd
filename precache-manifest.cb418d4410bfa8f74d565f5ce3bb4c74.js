418f826b51732e9a73fcea252dff6d3b1b6135b1
Ritu <ritu@ritu.co>
2020-04-05 13:25:11 +0530

    xd

---
+++ precache-manifest.cb418d4410bfa8f74d565f5ce3bb4c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa456ce00bb458048503fe9c56658caf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "530de924252caa0e1c96",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4ba592b51c60d0751ac",
+    "url": "/static/js/2.15baf818.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15baf818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "530de924252caa0e1c96",
+    "url": "/static/js/main.2b1c6562.chunk.js"
+  },
+  {
+    "revision": "48f9730497f9f4cd1745",
+    "url": "/static/js/runtime-main.15c74be5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
